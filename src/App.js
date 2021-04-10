38425372575a5fea40deef0d6524f6e29a9387e8
jkunciene <j.kunciene@gmail.com>
2021-04-10 15:47:57 +0300

    choreme dev tools redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
+  const dispatch = useDispatch();
+
+
   return (
     <div>
      <h1>Redux - state management</h1>
+     <p>Counter {counter}</p>
+
+     <button onClick={() => dispatch(increment())}>+</button>
+     <button onClick={() => dispatch(decrement())}>-</button>
+     {isLogged ? <h4>Valuable Information I shouldn't see</h4> : false}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,39 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
+import allReducers from './reducers';
 
-//STORE -> GLOBALIZED STATE
-//ACTION tik aparasau veiksmus
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-//REDUCER gauna veiksma is dispatch ir TIKRINA, ka reikia daryti
-const counter = (state=0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-};
-let store = createStore(counter);
-//Display it in the console
-store.subscribe(()=> console.log(store.getState()))
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
+import { Provider } from "react-redux"; //sujungia musu visus reducerius su App komponentu
+
+const store = createStore(
+        allReducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        );
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* //dabar musu informacija state tampa globali, pasiekiama visiems komponentams */}
+    <Provider store={store}>
+
+         <App />
+
+    </Provider>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
