6cdc7a1f64697b86f86e656af389674c2fc42e99
jkunciene <j.kunciene@gmail.com>
2021-04-10 13:55:19 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//STORE -> GLOBALIZED STATE
+//ACTION tik aparasau veiksmus
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+//REDUCER gauna veiksma is dispatch ir TIKRINA, ka reikia daryti
+const counter = (state=0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+let store = createStore(counter);
+//Display it in the console
+store.subscribe(()=> console.log(store.getState()))
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +39,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
